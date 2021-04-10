f8e077bf1578faef33dc23565bcf6f65b026176f
Arsalan <arsheikh665@gmail.com>
2021-04-10 23:12:10 +0500

    updated routes.js

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,6 +1,5 @@
-
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import NavBar from "./NavBar";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Footer from "./Footer";
 import Home from "../pages/Home";
 import About from "../pages/about";
@@ -15,7 +14,7 @@ export default function RoutesConfig(){
                 <Route path="/about" component={About}/>
                 <Route path="/weather" component={Weather}/>
 
-                <Route path="*" component={()=>{return <h1>404 page not found</h1>}}/>
+                <Route path="/*" component={()=>{return <h1>404 page not found</h1>}}/>
             </Switch>
             <Footer/>
         </Router>
